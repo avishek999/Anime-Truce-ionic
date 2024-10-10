70df4ed5fbd82aad651ac88df68ac0204325ef55
Avishek Prasad <avishekprasad0999@gmail.com>
2024-10-10 16:46:28 +0530

    newv

--- src/components/swipperBannerCard/SwipperCard.tsx
+++ src/components/swipperBannerCard/SwipperCard.tsx
@@ -2,7 +2,7 @@ import { IonRouterLink } from "@ionic/react";
 import React from "react";
 import { Swiper, SwiperSlide } from 'swiper/react'; // Corrected import
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
-import "./SwiperCard.scss";
+import "./SwippereCard.scss"
 import { HomeBannerProps } from "@/interface/Interface";
 
 // Import Swiper styles
