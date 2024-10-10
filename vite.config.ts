8bc7c46de2f4b6cf06862e0dc4cb43fd96f28ea8
Avishek Prasad <avishekprasad0999@gmail.com>
2024-10-10 14:59:19 +0530

    newSwip/mod/css

--- vite.config.ts
+++ vite.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
   },
   build: {
     rollupOptions: {
-      external: ['@capacitor/preferences','swiper/react','swiper/modules'],
+      external: ['@capacitor/preferences','swiper/react','swiper/modules','swiper/css'],
     },
   },
 })
