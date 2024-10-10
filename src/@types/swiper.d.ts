3c6eba214daeed5909bcc80a72f6aa11b2f8defa
Avishek Prasad <avishekprasad0999@gmail.com>
2024-10-10 14:35:26 +0530

    new

--- src/@types/swiper.d.ts
+++ src/@types/swiper.d.ts
@@ -7,6 +7,7 @@ declare module 'swiper/react' {
   declare module 'swiper/modules' {
     export const Autoplay:any;
     export const Pagination:any;
+    export const Navigation:any;
   }
 
   declare module '@capacitor/preferences' {
