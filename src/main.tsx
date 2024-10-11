c2cb36a1c1f81dec85f4abf76b72e941c04c7ab4
Avishek Prasad <avishekprasad0999@gmail.com>
2024-10-11 11:49:45 +0530

    remove strictmode

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './theme/variables.css'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./theme/variables.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container!);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(<App />);
